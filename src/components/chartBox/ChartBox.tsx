6bfb14e108fa51d00fb5e93a717bcc960570ea33
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-12-14 20:05:25 +0100

    fix random number generator

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -10,46 +10,47 @@ function ChartBox() {
   
 
 const [randomNumberList, setRandomNumberList] = useState<number[][]>([[0],[0]])
-function setRandomNumberListOfAList(index: number, value : number[]){
-    console.log("gggg")
-  let newRandomNumberList:  number[][] = JSON.parse(JSON.stringify(randomNumberList)) ;
-  newRandomNumberList[1][1] = 1 //.push(value) ; newRandomNumberList[index].length+1
-  
-  console.log("index", newRandomNumberList[index].length)
-
-  setRandomNumberList(()=> newRandomNumberList)
-  console.log(randomNumberList)
-  console.log(newRandomNumberList)
-}
- 
-
-let random = [[1,3,4], [3,9,7]]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   const [canGenerate, setCanGenerate] = useState(false);
 
-  const [minRange, setMinRange] = useState(0);
-  const [maxRange, setMaxRange] = useState(10);
-  const [timeInterval, setTimeInterval] = useState(1000);
-  const [open, setOpen] = useState(true)
-  const [numberOfChannels , setNumberOfChannels] = useState(2)
+  const [minRange, setMinRange] = useState<number>(0);
+  const [maxRange, setMaxRange] = useState<number>(10);
+  const [timeInterval, setTimeInterval] = useState<number>(1000);
+  const [open, setOpen] = useState<boolean>(true)
+  const [numberOfChannels , setNumberOfChannels] = useState<number>(2)
  const[channels, setChannels] = useState([])
+
+ const[test, setTest] = useState([0])
+
+// to generate the random numbers
+
+ useEffect(() => {
+      let intervalId : number;
+  
+      if (canGenerate) {
+        // Set up an interval to update the random number every 1000 milliseconds (1 second)
+        intervalId = setInterval(() => {
+
+          const newRandomNumberList = 
+
+          setRandomNumberList((prevRandomNumberList)=>prevRandomNumberList.map((subList)=>{
+            console.log("newRandomNumberList")
+          console.log(subList)
+            return [...subList, Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange ]}))
+          // setTest(()=>[...test, 3])
+
+        }, timeInterval);
+      }
+  
+      // Clean up the interval when the component is unmounted or when canGenerate is set to false
+      return () => clearInterval(intervalId);
+    }, [canGenerate]);
+
+
+
+
+  // To set the number of channels
+
   useEffect(() => {
     let newChannels = []
 
@@ -65,10 +66,10 @@ let random = [[1,3,4], [3,9,7]]
     setChannels( newChannels )
 }, [numberOfChannels]);
 
+// To save the generated numbers in to local storage
   useEffect(() => {
 localStorage.setItem('CHANNELS', JSON.stringify(channels));
-console.log(channels)
-console.log("saved")
+
 }, [canGenerate]);
  
 
@@ -79,15 +80,14 @@ setChannels(JSON.parse(localStorage.getItem('CHANNELS') ?? ""));
 }
 
 
-console.log(numberOfChannels)
-
   return (
 <>
+{test}
     <DialogBox loadSavedData={loadSavedData} numberOfChannels={numberOfChannels} setNumberOfChannels={setNumberOfChannels} open={open} setOpen={setOpen} setNumberOfChannels={setNumberOfChannels} />
     <Box sx={{ width: '100%' }}>
 
       {channels.map((channel, index)=>{
-        return <MyBarChart randomNumberList={randomNumberList[index]} setRandomNumberList={(v)=>{setRandomNumberListOfAList(index, v)}}  key={channel.index} timeInterval = {timeInterval} maxRange  = {maxRange} minRange = {minRange} canGenerate={canGenerate} index ={channel.index} color={channel.color} />
+        return <MyBarChart randomNumberList={randomNumberList[index]} key={channel.index} index ={channel.index} color={channel.color} />
       })}
 
 
